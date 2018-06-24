4c2c7e092a736c8766ceae9911a3da91d362f582
churabou <ginga7888@gmail.com>
2018-06-24 11:54:29 +0900

    set up

---
+++ Module/Views/Search/SearchRepositoryView.swift
@@ -0,0 +1,11 @@
+//
+//  SearchRepositoryView.swift
+//  GitHubSearch
+//
+//  Created by ちゅーたつ on 2018/06/24.
+//  Copyright © 2018年 ちゅーたつ. All rights reserved.
+//
+
+import UIKit
+
+
---
+++ Module/Views/Search/SearchRepositoryViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchRepositoryViewController.swift
+//  GitHubSearch
+//
+//  Created by ちゅーたつ on 2018/06/24.
+//  Copyright © 2018年 ちゅーたつ. All rights reserved.
+//
+
+import UIKit
+
+final class SearchRepositoryViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        view.backgroundColor = .orange
+    }
+}
